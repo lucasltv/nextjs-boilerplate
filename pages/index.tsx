140daf635313c66f0d2bd96d71b5a3941e520f3c
Lucas Vieira <lucas@devplace.com.br>
2021-04-23 20:54:19 -0300

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Vai filhao</h1>
+    </div>
+  );
+}
