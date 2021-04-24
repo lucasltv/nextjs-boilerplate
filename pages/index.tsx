5f4556564d510f4fb6054d65cd83bf724d935bbc
Lucas Vieira <lucas@devplace.com.br>
2021-04-23 21:12:57 -0300

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Vai filhao</h1>
+      <h2>TESTE: {process.env.NEXT_PUBLIC_TEST}</h2>
     </div>
   );
 }
